236e434d933ae5fbc23a4973cc3c175e25784407
Haffi Mazhar <haffimazhar96@gmail.com>
2024-02-21 23:20:32 +0000

    Initial commit

---
+++ tests/unit/test_apikeyservice_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from apikeyservice.apikeyservice_stack import ApikeyserviceStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in apikeyservice/apikeyservice_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ApikeyserviceStack(app, "apikeyservice")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
