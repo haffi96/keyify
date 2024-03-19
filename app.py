1f1ca26141707f86183020fca702beba6f61e93a
Haffi Mazhar <haffimazhar96@gmail.com>
2024-03-19 22:25:00 +0000

    dev container and stage name updates

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ prod_stage = cdk.Stage(
         account=settings.account,
         region=settings.region,
     ),
+    stage_name="prod",
 )
 
 dev_stage = cdk.Stage(
@@ -22,6 +23,7 @@ dev_stage = cdk.Stage(
         account=settings.account,
         region=settings.region,
     ),
+    stage_name="dev",
 )
 
 ApikeyserviceStack(
