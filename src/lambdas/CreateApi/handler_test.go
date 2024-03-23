7d2c42c704887c6454cc25b19a10da52470c9eb9
Haffi Mazhar <haffimazhar96@gmail.com>
2024-03-23 10:37:20 +0000

    feat: list keys route

---
+++ src/lambdas/CreateApi/handler_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"cfg"
+	"context"
+	"db"
+	"encoding/json"
+	"fmt"
+	"schemas"
+	"testing"
+	"utils"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateApiHandler(t *testing.T) {
+	ctx := context.Background()
+	d := CreateApiDeps{
+		DbClient:  db.GetMockDynamoClient(ctx),
+		TableName: cfg.Config.ApiKeyTable,
+	}
+
+	// Create rootKey row for testing
+	rootKey, _ := utils.GenerateApiKey("apikeyservice_")
+	hashedKey := utils.HashString(rootKey)
+	rootKeyReq := schemas.CreateRootKeyRequest{
+		WorkspaceId: "workspace-1",
+	}
+	db.CreateRootKeyRow(hashedKey, rootKeyReq, d.DbClient)
+
+	apiName := "my-test-api"
+
+	resp, err := d.handler(ctx, events.APIGatewayProxyRequest{
+		Headers: map[string]string{
+			"Authorization": "Bearer " + rootKey,
+		},
+		Body: fmt.Sprintf(`{"name":"%s"}`, apiName),
+	})
+
+	assert.Equal(t, nil, err, "Handler returned an error")
+	assert.Equal(t, 200, resp.StatusCode, "Expected status code 200")
+
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(resp.Body), &result)
+	if err != nil {
+		t.Fatalf("Unable to parse response body: %v", err)
+	}
+	if _, ok := result["apiId"]; !ok {
+		t.Errorf("Expected 'apiId' in response body, but not found")
+	}
+}
--- src/lambdas/ListApiKeys/handler.go
+++ src/lambdas/ListApiKeys/handler.go
@@ -4,13 +4,10 @@ import (
 	"auth"
 	"cfg"
 	"context"
+	"db"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-
-	"db"
-	"schemas"
 	"src"
 	"utils"
 
@@ -18,54 +15,44 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type GetApiKeyDeps src.Deps
+type ListApiKeysDeps src.Deps
 
 func main() {
-	d := GetApiKeyDeps{
+	d := ListApiKeysDeps{
 		DbClient:  db.GetDynamoClient(context.Background()),
 		TableName: cfg.Config.ApiKeyTable,
 	}
 	lambda.Start(d.handler)
 }
 
-func (d *GetApiKeyDeps) handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func (d *ListApiKeysDeps) handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Verify the request authentication
 	workspaceId, err := auth.VerifyAuthHeader(request, d.DbClient)
 	if err != nil {
 		return utils.HttpErrorResponse(http.StatusUnauthorized, fmt.Sprintf("Unauthorized: %s", err.Error())), nil
 	}
-	log.Printf("workspaceId: %s", workspaceId)
 
 	// Parse API ID and key ID from request parameters
 	apiId := request.QueryStringParameters["apiId"]
-	keyId := request.QueryStringParameters["apiKeyId"]
 
-	if apiId == "" || keyId == "" {
+	if apiId == "" {
 		return utils.HttpErrorResponse(http.StatusBadRequest, "Missing required query parameters: apiId and apiKeyId"), nil
 	}
 
-	apiKeyData, err := db.GetApiKey(workspaceId, apiId, keyId, d.DbClient)
-
+	// Query the database for all API keys for the workspace
+	keys, err := db.ListApiKeys(workspaceId, apiId, d.DbClient)
 	if err != nil {
-		return utils.HttpErrorResponse(http.StatusInternalServerError, fmt.Sprintf("Error getting API key: %v", err)), nil
+		return utils.HttpErrorResponse(http.StatusInternalServerError, fmt.Sprintf("Error listing API keys: %v", err)), nil
 	}
 
-	respBody := schemas.GetApiKeyResponse{
-		ApiId:  apiId,
-		KeyId:  keyId,
-		Name:   apiKeyData.Name,
-		Prefix: apiKeyData.Prefix,
-		Roles:  apiKeyData.Roles,
-	}
-	respBodyJSON, err := json.Marshal(respBody)
-
+	// Construct the response
+	keysJson, err := json.Marshal(keys)
 	if err != nil {
-		return utils.HttpErrorResponse(http.StatusInternalServerError, fmt.Sprintf("Error marshalling response: %v", err)), nil
+		return utils.HttpErrorResponse(http.StatusInternalServerError, fmt.Sprintf("Error marshalling keys: %v", err)), nil
 	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
-		Body:       string(respBodyJSON),
+		Body:       string(keysJson),
 	}, nil
-
 }
